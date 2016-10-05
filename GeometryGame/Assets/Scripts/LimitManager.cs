9aed3d9acf5b159ba2bf7e041611c6b6845369b4
Mendes Raphael <r.mendes@student.isartdigital.com>
2016-10-05 15:51:16 +0200

    Gestion des limit de la camera

---
+++ GeometryGame/Assets/Scripts/LimitManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LimitManager : MonoBehaviour
+    {
+        private int limitPos = -450;
+
+        protected void Start()
+        {
+
+        }
+
+        protected void Update()
+        {
+            if(transform.TransformPoint(transform.position).x <= limitPos)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ GeometryGame/Assets/Scripts/LimitsManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
