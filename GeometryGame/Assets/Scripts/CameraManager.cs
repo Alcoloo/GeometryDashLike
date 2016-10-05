09dcd35ba86be0f44ed161b9cb0a67b932458ca3
Mendes Raphael <r.mendes@student.isartdigital.com>
2016-10-05 15:10:28 +0200

    Score + UI

--- GeometryGame/Assets/Scripts/CameraManager.cs
+++ GeometryGame/Assets/Scripts/CameraManager.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class CameraManager : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject floor;
+	public GameObject floorMovement;
+	private float cameraXPos;
 
 	private float offsetX;
 	// Use this for initialization
 	void Start () {
 		offsetX = transform.position.x - player.transform.position.x;
+		cameraXPos = transform.position.x - (Screen.width / 2);
 	}
+		
 	
 	// Update is called once per frame
 	void LateUpdate () {
